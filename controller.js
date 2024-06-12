65336a0e9916665f59c4af8f6f9ed46574e29e47
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 16:33:52 +0700

    Tampil semua data kategori dan berdasarkan id

--- controller.js
+++ controller.js
@@ -6,3 +6,30 @@ var connection = require("./koneksi"); // connection menampung data file koneksi
 exports.index = function (req, res) {
   response.ok("Aplikasi Rest API berjalan!", res); // response.ok(mengambil properti oke dari res.js) value nya yang ada di dalam kurung
 };
+
+//Tampilkan Semua Data Kategori
+exports.tampilKategori = function (req, res) {
+  connection.query("SELECT * FROM kategori", function (error, rows, fileds) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res); //
+    }
+  });
+
+  //Tampilkan Data Kategori Berdasarkan id_kategori
+  exports.tampilKategoriBerdasarkanId_kategori = function (req, res) {
+    let IdKategori = req.params.IdKategori;
+    connection.query(
+      "SELECT * FROM kategori WHERE id_kategori =?",
+      { IdKategori },
+      function (error, rows, fileds) {
+        if (error) {
+          console.log(error);
+        } else {
+          response.ok(rows, res);
+        }
+      }
+    );
+  };
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function (app) {
   var myJson = require("./controller"); // myjson menampung data dari controller.js
 
   app.route("/").get(myJson.index); //ketika url "./" maka get index yang ada di myjson(conrtroller.js)
+
+  app.route("/kategori").get(myJson.tampilKategori); //ketika url "./kategori" maka get tampilKategori dari controller.js.
 };

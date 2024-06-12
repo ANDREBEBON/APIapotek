012731bb1d3632ef56a567c5afe7db94184af6b1
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-13 00:10:04 +0700

    Update dataController.js

--- controller/dataController.js
+++ controller/dataController.js
@@ -13,18 +13,18 @@ exports.kategoriberdasarid = function (req, res) {
   let id = req.params.id;
   const query = `
      SELECT 
-    COALESCE(NULLIF(o.namaObat, ''), NULL) AS namaObat,
-    COALESCE(NULLIF(o.gambarObat, ''), NULL) AS gambarObat,
-    COALESCE(NULLIF(o.hargaObat, 0), NULL) AS hargaObat,
-    COALESCE(NULLIF(o.deskripsiObat, ''), NULL) AS deskripsiObat,
-    COALESCE(NULLIF(v.namaVitamin, ''), NULL) AS namaVitamin,
-    COALESCE(NULLIF(v.gambarVitamin, ''), NULL) AS gambarVitamin,
-    COALESCE(NULLIF(v.hargaVitamin, 0), NULL) AS hargaVitamin,
-    COALESCE(NULLIF(v.deskripsiVitamin, ''), NULL) AS deskripsiVitamin,
-    COALESCE(NULLIF(s.namaSuplemen, ''), NULL) AS namaSuplemen,
-    COALESCE(NULLIF(s.gambarSuplemen, ''), NULL) AS gambarSuplemen,
-    COALESCE(NULLIF(s.hargaSuplemen, 0), NULL) AS hargaSuplemen,
-    COALESCE(NULLIF(s.deskripsiSuplemen, ''), NULL) AS deskripsiSuplemen
+    COALESCE(NULLIF(o.namaObat, ''), 
+             NULLIF(v.namaVitamin, ''), 
+             NULLIF(s.namaSuplemen, '')) AS nama,
+    COALESCE(NULLIF(o.gambarObat, ''), 
+             NULLIF(v.gambarVitamin, ''), 
+             NULLIF(s.gambarSuplemen, '')) AS gambar,
+    COALESCE(NULLIF(o.hargaObat, 0), 
+             NULLIF(v.hargaVitamin, 0), 
+             NULLIF(s.hargaSuplemen, 0)) AS harga,
+    COALESCE(NULLIF(o.deskripsiObat, ''), 
+             NULLIF(v.deskripsiVitamin, ''), 
+             NULLIF(s.deskripsiSuplemen, '')) AS deskripsi
 FROM 
     kategori k
 LEFT JOIN 
@@ -34,21 +34,22 @@ LEFT JOIN
 LEFT JOIN 
     suplemen s ON k.id_kategori = s.id_kategori
 WHERE 
-    k.id_kategori = 3
+    k.id_kategori = ?
     AND (
         (o.namaObat IS NOT NULL AND o.namaObat <> '') OR
-        (o.gambarObat IS NOT NULL AND o.gambarObat <> '') OR
-        (o.hargaObat IS NOT NULL AND o.hargaObat <> 0) OR
-        (o.deskripsiObat IS NOT NULL AND o.deskripsiObat <> '') OR
         (v.namaVitamin IS NOT NULL AND v.namaVitamin <> '') OR
-        (v.gambarVitamin IS NOT NULL AND v.gambarVitamin <> '') OR
-        (v.hargaVitamin IS NOT NULL AND v.hargaVitamin <> 0) OR
-        (v.deskripsiVitamin IS NOT NULL AND v.deskripsiVitamin <> '') OR
         (s.namaSuplemen IS NOT NULL AND s.namaSuplemen <> '') OR
+        (o.gambarObat IS NOT NULL AND o.gambarObat <> '') OR
+        (v.gambarVitamin IS NOT NULL AND v.gambarVitamin <> '') OR
         (s.gambarSuplemen IS NOT NULL AND s.gambarSuplemen <> '') OR
-        (s.hargaSuplemen IS NOT NULL AND s.hargaSuplemen <> 0) OR
+        (o.hargaObat <> 0) OR
+        (v.hargaVitamin <> 0) OR
+        (s.hargaSuplemen <> 0) OR
+        (o.deskripsiObat IS NOT NULL AND o.deskripsiObat <> '') OR
+        (v.deskripsiVitamin IS NOT NULL AND v.deskripsiVitamin <> '') OR
         (s.deskripsiSuplemen IS NOT NULL AND s.deskripsiSuplemen <> '')
-    )
+    );
+
 
 `;
   connection.query(query, [id], function (error, rows, fields) {

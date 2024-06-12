dd7f78ca08544cd74c262cd3a61fc8509839c8c9
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 16:36:33 +0700

    Tampil semua data kategori dan berdasarkan id

--- routes.js
+++ routes.js
@@ -6,4 +6,6 @@ module.exports = function (app) {
   app.route("/").get(myJson.index); //ketika url "./" maka get index yang ada di myjson(conrtroller.js)
 
   app.route("/kategori").get(myJson.tampilKategori); //ketika url "./kategori" maka get tampilKategori dari controller.js.
+
+  app.route("/kategori/:id").get(myJson.tampilKategoriBerdasarkanId_kategori); //ketika url "./kategori/:id maka get data kategori berdasarkan id
 };

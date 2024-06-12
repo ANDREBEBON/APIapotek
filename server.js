cd9765dac7008b3d892550f7e8f4aabf744ffdc9
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 15:09:56 +0700

    Membuat Koneksi di file koneksi.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+
+//Parse Aplikasi format json
+app.use(bodyParser.urlencoded({extended: true})); //bodyParser.url
+app.use(bodyParser.json()); //bodyParser.json
+app.listen(3001, () => {
+    console.log(`Server started on port`);
+});
+

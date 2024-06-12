cd82f60207929dd5c79d341af56842ad197d4549
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 15:37:09 +0700

    tambah coment agar lebih mudah di pahami

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 "use strict";
 
-var response = require("./res");
-var connection = require("./koneksi");
+var response = require("./res"); // reponse menampung data file res.js
+var connection = require("./koneksi"); // connection menampung data file koneksi.js
 
 exports.index = function (req, res) {
-  response.ok("Aplikasi Rest API berjalan!");
+  response.ok("Aplikasi Rest API berjalan!"); // response.ok(mengambil properti oke dari res.js) value nya yang ada di dalam kurung
 };
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = function (app) {
-  var myJson = require("./controller");
+  var myJson = require("./controller"); // myjson menampung data dari controller.js
 
-  app.route("./").get(myJson.index);
+  app.route("./").get(myJson.index); //ketika url "./" maka get index yang ada di myjson(conrtroller.js)
 };

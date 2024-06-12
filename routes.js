95c7b0414557e5662c3b7e37007e82234440866e
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 21:52:17 +0700

    Tambah LoginRegister

--- routes.js
+++ routes.js
@@ -4,9 +4,11 @@ module.exports = function (app) {
   var myJson = require("./controller");
   var dataController = require("./controller/dataController");
   var registerController = require("./controller/registerController");
+  var loginController = require("./controller/loginController");
 
   app.route("/").get(myJson.index);
   app.route("/kategori").get(myJson.tampilKategori);
   app.route("/kategori/:id").get(dataController.kategoriberdasarid);
   app.route("/register").post(registerController.dataRegister);
+  app.route("/login").post(loginController.dataLogin);
 };

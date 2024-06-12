6722949eadbe9854a4df4afebb803a6ef06f0f16
ANDREBEBON <andrebebon@students.amikom.ac.id>
2024-06-12 15:39:57 +0700

    seting routes.js di server.js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
+//panggil routes.js
+var routes = require("./routes");
+routes(app);
+
 //Parse Aplikasi format json
-app.use(bodyParser.urlencoded({extended: true})); //bodyParser.url
+app.use(bodyParser.urlencoded({ extended: true })); //bodyParser.url
 app.use(bodyParser.json()); //bodyParser.json
 app.listen(3001, () => {
-    console.log(`Server started on port`);
+  console.log(`Server started on port`);
 });
-
